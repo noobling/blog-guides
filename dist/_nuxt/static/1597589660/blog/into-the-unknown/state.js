4c4a6432d03c00f1308d734b6016828c064e47d2
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-08-17 06:49:47 +0800

    remove dist directory and add to gitignore

--- dist/_nuxt/static/1597589660/blog/into-the-unknown/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1597589660",layout:"default",error:null,serverRendered:a,routePath:"\u002Fblog\u002Finto-the-unknown",config:{},colorMode:{preference:b,value:b,unknown:a}}}(true,"system"));
