a01acacadef7ed8677907c777252d9d2531d725f
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-08-24 22:32:32 +0800

    minor changes for search

--- components/Header/Search.vue
+++ components/Header/Search.vue
@@ -1,10 +1,13 @@
 <template>
-  <input
-    type="search"
-    name="search"
-    placeholder="Search Articles"
-    class="w-10 sm:w-56 search pl-6 sm:pl-10 pr-4 placeholder-black opacity-50 focus:opacity-100"
-  />
+  <div class="w-10 sm:w-56 search">
+    <input
+      type="search"
+      name="search"
+      title="Search Articles"
+      placeholder="Search Articles"
+      class="search-bar pl-6 sm:pl-10 pr-4 placeholder-black sm:opacity-50 focus:opacity-100"
+    />
+  </div>
 </template>
 
 <script>
@@ -13,14 +16,17 @@ export default {}
 
 <style scoped>
 .search {
+  @apply h-10 absolute right-0 z-40;
+}
+.search-bar {
   background: url('~assets/search-icon.svg') no-repeat 0.5rem center;
   background-size: 1.5rem;
   transition: all 0.5s;
-  @apply h-10 border-white bg-white text-primary py-2 absolute right-0 z-40;
+  @apply h-full w-full border-white bg-white text-primary py-2 absolute right-0 z-40;
 }
 
 @media (max-width: 576px) {
-  .search:focus {
+  .search-bar:focus {
     width: calc(100vw - 2rem);
     @apply pl-10;
   }
